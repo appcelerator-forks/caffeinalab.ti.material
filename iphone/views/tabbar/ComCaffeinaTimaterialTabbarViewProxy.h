015df95a1a797e50dc39b6e51ce3422767b99787
Ani Sinanaj <ani.sinanaj@caffeina.com>
2017-02-16 11:23:04 +0100

    Adding TabbarView

---
+++ iphone/views/tabbar/ComCaffeinaTimaterialTabbarViewProxy.h
@@ -0,0 +1,14 @@
+//
+//  ComCaffeinaTimaterialTabBarProxy.h
+//  ti.material
+//
+//  Created by Ani Sinanaj on 13/02/2017.
+//
+//
+
+#import "TiViewProxy.h"
+
+@interface ComCaffeinaTimaterialTabbarViewProxy : TiViewProxy {
+    @private
+}
+@end
